acc7302313ff9da90def329e979e5155ff11aa8c
Christian <chris.346@hotmail.com>
2025-08-28 17:42:18 -0400

    added github pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/personalplayer_front/", // Replace with your repo name
   plugins: [react()],
   server: {
     port: 3000, // Change the port number to 3000
-  }
+  },
 });
